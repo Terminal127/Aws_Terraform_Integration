6fd0ae603d743555bd3015acb9507d8c7219b652
Terminal127 <terminalishere127@gmail.com>
2023-11-02 09:32:00 +0530

    Readme

--- instance.tf
+++ instance.tf
@@ -1,3 +1,45 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:63e37f63c3b6896370a23b87ec52ba6823dc1ab0336be9b2639b2c335c6143ca
-size 1015
+# creating ssh-key
+resource "aws_key_pair" "key-tf" {
+  key_name   = "key-tf"
+  public_key = file("${path.module}/id_rsa.pub")
+}
+
+#creating security key
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  dynamic "ingress" {
+    for_each = [22, 80, 443, 3306, 27017]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+}
+
+
+# Creating instance
+resource "aws_instance" "web" {
+  ami                    = "Your Preffered OS"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key-tf.key_name
+  vpc_security_group_ids = ["${aws_security_group.allow_tls.id}"]
+  tags = {
+    Name = "First-tf-instance"
+  }
+}
