fae43165f857fd32deb410b33e6ee7a63832aab6
Terminal127 <terminalishere127@gmail.com>
2023-11-02 09:04:40 +0530

    Pirvacy Changes

--- provider.tf
+++ provider.tf
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d52fade5b1f31394f97a7bdea9b2c2d38bac6d533bd0bf23d9e35f3933911a74
-size 168
+oid sha256:8107fa9c39c2785271af1e84521bc1b71170601839eca6524f594819656553f6
+size 140
